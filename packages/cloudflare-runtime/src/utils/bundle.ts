d58bc7a0fee8d719968bd342aeb29a3bf2f3a82f
John Royal <34844819+john-royal@users.noreply.github.com>
2026-04-12 17:55:14 -0700

    add bundle watch

--- packages/cloudflare-runtime/src/utils/bundle.ts
+++ packages/cloudflare-runtime/src/utils/bundle.ts
@@ -1,15 +1,19 @@
 import cloudflare from "@distilled.cloud/cloudflare-rolldown-plugin";
-import { flow } from "effect";
 import * as Effect from "effect/Effect";
-import { rolldown } from "rolldown";
+import { flow } from "effect/Function";
+import * as Queue from "effect/Queue";
+import * as Stream from "effect/Stream";
+import * as Rolldown from "rolldown";
+
+type Output = [Rolldown.OutputChunk, ...Array<Rolldown.OutputChunk | Rolldown.OutputAsset>];
 
 export const bundle = Effect.fn((entry: string) =>
-  Effect.promise(async () => {
-    const bundle = await rolldown({
+  Effect.promise(async (): Promise<Output> => {
+    const bundle = await Rolldown.rolldown({
       input: entry,
       plugins: [cloudflare()],
     });
-    const output = await bundle.generate({
+    const { output } = await bundle.generate({
       file: "worker.js",
       format: "esm",
       minify: false,
@@ -19,10 +23,36 @@ export const bundle = Effect.fn((entry: string) =>
   }),
 );
 
-export const bundleAsEsModule = flow(
-  bundle,
-  Effect.map(({ output }) => ({
-    name: "worker.js",
-    esModule: output[0].code,
-  })),
-);
+const transformRolldownOutput = (output: Output) => ({
+  name: "worker.js",
+  esModule: output[0].code,
+});
+
+export const bundleAsEsModule = flow(bundle, Effect.map(transformRolldownOutput));
+
+export const watch = (entry: string) =>
+  Stream.callback<Output>((queue) =>
+    Effect.acquireRelease(
+      Effect.sync(() => {
+        const watcher = Rolldown.watch({
+          input: entry,
+          plugins: [
+            cloudflare(),
+            {
+              name: "alchemy:watch-bundle",
+              generateBundle(_outputOptions, bundle) {
+                Queue.offerUnsafe(queue, Object.values(bundle) as Output);
+              },
+            },
+          ],
+          output: {
+            file: "worker.js",
+            format: "esm",
+            minify: false,
+          },
+        });
+        return watcher;
+      }),
+      (watcher) => Effect.promise(() => watcher.close()),
+    ),
+  ).pipe(Stream.map(transformRolldownOutput));

88b69205add43010cf4944b0be8c5ed5a9a3c53d
John Royal <34844819+john-royal@users.noreply.github.com>
2026-05-13 20:21:04 -0400

    update configs

--- fixtures/static-website/vite.config.ts
+++ fixtures/static-website/vite.config.ts
@@ -1,36 +1,8 @@
+import cloudflare from "@distilled.cloud/cloudflare-vite-plugin";
 import { defineConfig } from "vite";
 
-const cloudflare = await import("@cloudflare/vite-plugin").then((m) => m.cloudflare);
-const distilled = await import("@distilled.cloud/cloudflare-vite-plugin").then((m) => m.default);
-
 const config = defineConfig({
-  plugins: [
-    // distilled({
-    //   main: "./src/server.ts",
-    //   compatibilityDate: undefined,
-    //   compatibilityFlags: undefined,
-    // }),
-    cloudflare({
-      config: {
-        main: "./src/server.ts",
-        compatibility_date: undefined,
-        compatibility_flags: undefined,
-      },
-      viteEnvironment: {
-        name: "ssr",
-      },
-    }),
-    // {
-    //   name: "distilled-cloudflare:info",
-    //   configResolved(config) {
-    //     console.log(this.meta);
-    //     fs.writeFileSync("config-distilled.json", JSON.stringify(config, null, 2));
-    //   },
-    //   async buildApp(app) {
-    //     console.log(app.environments);
-    //   },
-    // },
-  ],
+  plugins: [cloudflare({})],
 });
 
 export default config;
--- fixtures/tanstack-start/vite.config.ts
+++ fixtures/tanstack-start/vite.config.ts
@@ -1,42 +1,18 @@
 import { devtools } from "@tanstack/devtools-vite";
 import { defineConfig } from "vite";
 
-import { layerRuntime } from "@distilled.cloud/cloudflare-runtime";
 import distilled from "@distilled.cloud/cloudflare-vite-plugin";
-import * as Credentials from "@distilled.cloud/cloudflare/Credentials";
-import * as NodeServices from "@effect/platform-node/NodeServices";
-import * as Effect from "effect/Effect";
-import * as Layer from "effect/Layer";
-import * as Scope from "effect/Scope";
-import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 
-const scope = Scope.makeUnsafe();
-
-const context = await layerRuntime({
-  server: {
-    port: 0,
-    host: "localhost",
-  },
-  api: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-  },
-}).pipe(
-  Layer.provide(Layer.mergeAll(Credentials.fromEnv(), NodeServices.layer, FetchHttpClient.layer)),
-  Layer.buildWithScope(scope),
-  Effect.runPromise,
-);
-
 const config = defineConfig({
   plugins: [
     distilled({
       compatibilityDate: "2026-03-10",
       compatibilityFlags: ["nodejs_compat"],
-      context,
     }),
     devtools(),
     tailwindcss(),

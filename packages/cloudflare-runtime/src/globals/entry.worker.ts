42765c3f8ab3bdbf836ad11aaaedfdcb4a467880
John Royal <34844819+john-royal@users.noreply.github.com>
2026-05-18 16:41:35 -0400

    fix type error

--- packages/cloudflare-runtime/src/globals/entry.worker.ts
+++ packages/cloudflare-runtime/src/globals/entry.worker.ts
@@ -1,3 +1,4 @@
+import type { Fetcher } from "@cloudflare/workers-types/experimental";
 import { makeErrorResponse } from "../internal/response.shared.ts";
 import { SystemError } from "../RuntimeError.shared.ts";
 

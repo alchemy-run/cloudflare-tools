6fe78e07125a77afbdda3a1aad31f39bc80bf987
John Royal <34844819+john-royal@users.noreply.github.com>
2026-05-28 01:32:01 -0400

    fix test file resolution

--- packages/cloudflare-vite-plugin/vitest.config.ts
+++ packages/cloudflare-vite-plugin/vitest.config.ts
@@ -1,7 +1,13 @@
+import path from "node:path";
 import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "#": path.resolve(import.meta.dirname, "src"),
+    },
+  },
   test: {
     env: loadEnv("test", process.cwd(), "TEST"),
     include: ["test/**/*.test.ts"],

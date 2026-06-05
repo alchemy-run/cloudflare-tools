9ac180fbb98ae1fdbeabf9727702772a89d8a003
John Royal <34844819+john-royal@users.noreply.github.com>
2026-06-05 16:02:57 -0400

    clean up vite config for tanstack start example

--- fixtures/tanstack-start/vite.config.ts
+++ fixtures/tanstack-start/vite.config.ts
@@ -1,32 +1,17 @@
+import { Text } from "@distilled.cloud/cloudflare-runtime/bindings";
+import cloudflare from "@distilled.cloud/cloudflare-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import { defineConfig } from "vite";
-
-// import { cloudflare } from "@cloudflare/vite-plugin";
-import { Text } from "@distilled.cloud/cloudflare-runtime/bindings";
-import distilled from "@distilled.cloud/cloudflare-vite-plugin";
-
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
-import tailwindcss from "@tailwindcss/vite";
-import viteReact from "@vitejs/plugin-react";
 
 dotenv.config();
 
 const config = defineConfig({
   plugins: [
-    // cloudflare({
-    //   config: {
-    //     compatibility_date: "2026-03-10",
-    //     compatibility_flags: ["nodejs_compat"],
-    //     main: "@tanstack/react-start/server-entry",
-    //     vars: {
-    //       TEST_POSTGRES_URL: process.env.TEST_POSTGRES_URL!,
-    //     },
-    //   },
-    //   viteEnvironment: { name: "ssr" },
-    // }),
-    distilled({
+    cloudflare({
       compatibilityDate: "2026-03-10",
       compatibilityFlags: ["nodejs_compat"],
       worker: {

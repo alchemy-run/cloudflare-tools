d07bb18004c9c20b6ba8e73dc549abbdfbdde4b2
John Royal <34844819+john-royal@users.noreply.github.com>
2026-06-25 11:41:14 -0400

    remove test fixtures

--- fixtures/react-router-rsc/react-router-vite/entry.rsc.single.tsx
+++
@@ -1,8 +0,0 @@
-import type * as EntrySsr from "./entry.ssr";
-import { fetchServer } from "./entry.rsc";
-
-export default async function handler(request: Request) {
-  const ssr = await import.meta.viteRsc.loadModule<EntrySsr>("ssr", "index");
-
-  return ssr.default(request, await fetchServer(request));
-}
